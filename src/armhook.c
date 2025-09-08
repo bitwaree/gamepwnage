8cbe6274379be42d0f92c0623795fa871af78b08
bitwaree <bitware@duck.com>
2025-09-09 01:11:40 +0530

    updated armhook.c, implemented IP registers.

--- src/armhook.c
+++ src/armhook.c
@@ -19,8 +19,8 @@
 uintptr_t __attribute__((visibility(VISIBILITY_FLAG))) arm_hook64(uintptr_t AddresstoHook, uintptr_t hookFunAddr, size_t len)
 {
     const uint32_t nopBytes = 0xd503201f; // nop in aarch64
-    const uint32_t popBytes = 0xf85f83e0; // ldr x0, [sp, #-8]
-    const uint32_t shHookCode[4] = { 0xf81f83e0, 0x10000060, 0xf9400000, 0xd61f0000 };
+    const uint32_t popBytes = 0xf85f83f1; // ldr x17, [sp, #-8]
+    const uint32_t shHookCode[4] = { 0xf81f83f1, 0x10000071, 0xf9400231, 0xd61f0220 };
 
     if (len%4 != 0 || len<28)
     {
@@ -52,8 +52,8 @@ uintptr_t __attribute__((visibility(VISIBILITY_FLAG))) arm_hook64(uintptr_t Addr
 uintptr_t __attribute__((visibility(VISIBILITY_FLAG))) arm_hook32(uintptr_t AddresstoHook, uintptr_t hookFunAddr, size_t len)
 {
     const uint32_t nopBytes = 0xe1a00000; // nop in arm
-    const uint32_t popBytes = 0xe51d0004; // ldr r0, [sp, #-4]
-    const uint32_t shHookCode[3] = { 0xe50d0004, 0xe59f0000, 0xe12fff10 };
+    const uint32_t popBytes = 0xe51dc004; // ldr r12, [sp, #-4]
+    const uint32_t shHookCode[3] = { 0xe50dc004, 0xe59fc000, 0xe12fff1c };
 
     if (len%4 != 0 || len<20)
     {
@@ -69,7 +69,7 @@ uintptr_t __attribute__((visibility(VISIBILITY_FLAG))) arm_hook32(uintptr_t Addr
 
     //overlay shellcode
     memcpy(pseudomem, shHookCode, 12);
-    *((uint32_t*)(pseudomem + 12)) = hookFunAddr & 0xFFFFFFFF;          // copy the 64 bit address
+    *((uint32_t*)(pseudomem + 12)) = hookFunAddr & 0xFFFFFFFF;          // copy the 32 bit address
     *((uint32_t*)(pseudomem + 16)) = popBytes & 0xFFFFFFFF;             // copy the 32 bit pop opcodes
 
     if(write_mem((void *) AddresstoHook, (void *) pseudomem, len) != 1)
