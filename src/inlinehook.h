38a255533dedf56e80368d09290018b9dd1c51b6
bitwaree <bitware@duck.com>
2025-09-28 23:46:33 +0530

    added multiple hooking modes and flags for aarch64

--- src/inlinehook.h
+++ src/inlinehook.h
@@ -14,7 +14,17 @@ typedef struct {
     void *address;          // where to place hook
     void *fake;             // the fake function
     void *trampoline_addr;  // allocated address to place the copied bytes
+    int flags;              // flags
 } hook_handle;
 
-hook_handle* __attribute__((visibility(VISIBILITY_FLAG))) hook_addr(void *address, void *fake, void **original_func);
+#if defined(__aarch64__)
+#define GPWN_AARCH64_LEGACYHOOK 0x1 /* 5 instructions, 20 bytes */
+#define GPWN_AARCH64_MICROHOOK  0x2 /* 3 instructions, 12 bytes */
+#define GPWN_AARCH64_NANOHOOK   0x3 /* 1 instructions, 4 bytes  */
+#elif defined(__arm__)
+#define GPWN_ARM_LEGACYHOOK     0x1 /* 3 instructions, 12 bytes */
+#define GPWN_ARM_NANOHOOK       0x2 /* 1 instructions, 4 bytes  */
+#endif
+
+hook_handle* __attribute__((visibility(VISIBILITY_FLAG))) hook_addr(void *address, void *fake, void **original_func, int flags);
 bool __attribute__((visibility(VISIBILITY_FLAG))) rm_hook(hook_handle *handle);
