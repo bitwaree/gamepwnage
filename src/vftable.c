19a899b36f39e7738cf6f70e926403a90969e6f9
bitwaree <bitware@duck.com>
2026-03-28 13:06:42 +0530

    added vftable.c

---
+++ src/vftable.c
@@ -0,0 +1,40 @@
+/*
+ gamepwnage -- Cross Platform Game Hacking API(s)
+ Copyright (c) 2024-2026 bitware. All rights reserved.
+
+ "gamepwnage" is released under the New BSD license (see LICENSE.txt).
+ Go to the project home page for more info:
+ https://github.com/bitwaree/gamepwnage
+ */
+
+#ifdef GPWN_USING_BUILD_CONFIG
+#include "config.h"
+#else
+#ifndef GPWNAPI
+#define GPWNAPI
+#endif
+#ifndef GPWN_BKND
+#define GPWN_BKND
+#endif
+#endif
+
+#include <stdio.h>
+#include <string.h>
+
+#include "vftable.h"
+#include "dynlib.h"
+
+
+GPWNAPI void *get_vftable_ptr(const char *libname, const char *classname) {
+    char sym[4096];
+    size_t class_len = strnlen(classname, 4096);
+    if(
+        !classname || !*classname ||
+        class_len >= 4096 - 7) {
+        return 0;   // symbol too long
+    }
+
+    sprintf(sym, "_ZTV%zu%s", class_len, classname);
+    void *vtable_addr = gpwn_dlsym(libname, (const char*) sym);
+    return (vtable_addr) ? vtable_addr + 2*sizeof(void*) : 0;
+}
